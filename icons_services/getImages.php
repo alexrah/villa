39c4d626cb9e51f7a211ad287c790cdb92e2b294
Ale Sandro <alexrah@gmail.com>
2014-08-27 02:34:48 +0200

    design: icon gallery files

---
+++ icons_services/getImages.php
@@ -0,0 +1,15 @@
+<?php
+// No direct access to this file
+// defined('_JEXEC') or die('Restricted access');
+
+$filenameArray = array();
+
+$handle = opendir(dirname(realpath(__FILE__)).'/icons/');
+        while($file = readdir($handle)){
+            if($file !== '.' && $file !== '..'){
+                array_push($filenameArray, "icons_services/icons/$file");
+            }
+        }
+
+echo json_encode($filenameArray);
+?>
